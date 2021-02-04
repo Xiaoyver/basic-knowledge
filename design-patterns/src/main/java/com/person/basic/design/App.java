eb25586c57de79d6e213ed211022494f65b63e2e
gdLiu <ixiaoyver@gmail.com>
2021-02-04 10:36:07 +0800

    设计模式初始化

---
+++ design-patterns/src/main/java/com/person/basic/design/App.java
@@ -0,0 +1,12 @@
+package com.person.basic.design;
+
+/**
+ * Hello world!
+ *
+ * @author manjaro
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
